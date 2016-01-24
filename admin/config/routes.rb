5bd805f98b9ccb8ea3dd0e0a3efb7d6a87bba12f
Rafael Chacón <rafaelchacon@gmail.com>
2016-01-23 17:36:01 -0800

    Remove namespace, it's redudant in this context.

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
-
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
+  DashboardManifest::DASHBOARDS.each do |dashboard_resource|
+    resources dashboard_resource
   end
+  root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
 end
