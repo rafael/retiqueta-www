9204feb9a361d60538140825448c60b1df89f3e4
Rafael Chacón <rafaelchacon@gmail.com>
2016-06-20 22:03:14 -0700

    Add fake product_picture_ids

--- admin/app/models/product.rb
+++ admin/app/models/product.rb
@@ -5,5 +5,9 @@ class Product < ActiveRecord::Base
   ##################
 
   belongs_to :user, primary_key: :uuid
-  has_many :product_pictures, -> { order(position: :asc) }, primary_key: :uuid
+  has_many :product_pictures, -> { order(osition: :asc) }, primary_key: :uuid
+
+  def product_picture_ids
+    product_pictures.map(&:id)
+  end
 end
