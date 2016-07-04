e15c06f7d6c349a4621097ef2fd876da3c5b7be7
Rafael Chacón <rafaelchacon@gmail.com>
2016-07-03 21:00:24 -0700

    Add sidekiq to admin

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,8 +1,15 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   scope module: 'admin', as: 'admin' do
     DashboardManifest::DASHBOARDS.each do |dashboard_resource|
       resources dashboard_resource
     end
     root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
+
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      username == ENV["ADMIN_USER"] && password == ENV["ADMIN_PASSWORD"]
+    end
+    mount Sidekiq::Web => '/jobs/sidekiq'
   end
 end
