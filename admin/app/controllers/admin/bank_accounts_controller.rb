06be8dcf94c0e5e331626697e39f2627865cfd00
Rafael Chacón <rafaelchacon@gmail.com>
2016-08-15 21:07:38 -0700

    Search by username

--- admin/app/controllers/admin/bank_accounts_controller.rb
+++ admin/app/controllers/admin/bank_accounts_controller.rb
@@ -3,10 +3,24 @@ module Admin
     # To customize the behavior of this controller,
     # simply overwrite any of the RESTful actions. For example:
     #
-    # def index
-    #   super
-    #   @resources = BankAccount.all.paginate(10, params[:page])
-    # end
+     def index
+       search_term = params[:search].to_s.strip
+       resources = nil
+       if search_term && user = User.find_by_username(search_term)
+         resources = BankAccount.where(profile_id: user.profile.id).page(params[:page]).per(20)
+       elsif search_term != ""
+         resources = BankAccount.where(id: nil).page(0).per(0)
+       else
+         resources = BankAccount.all.page(params[:page]).per(20)
+       end
+       resources = order.apply(resources)
+       page = Administrate::Page::Collection.new(dashboard, order: order)
+       render locals: {
+                resources: resources,
+                search_term: search_term,
+                page: page,
+              }
+     end
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
