6afd89d251a25244144f88708b9729d5949f851a
Rafael Chacón <rafaelchacon@gmail.com>
2016-08-30 23:23:46 -0700

    Add tos

--- landing/frontend/src/components/Footer/Footer.js
+++ landing/frontend/src/components/Footer/Footer.js
@@ -18,9 +18,14 @@ const Footer = () => (
       </div>
     </div>
 
+    <div className="tos">
+    <a href='http://www.retiqueta.com/tos.html'>Términos y Condiciones</a>
+    </div>
+    
     <div className="mix-panel">
     <a href='https://mixpanel.com/f/partner'><img src='https://cdn.mxpnl.com/site_media/images/partner/badge_blue.png' alt='Mobile Analytics' /></a>
     </div>
+
   </footer>
 )
 
